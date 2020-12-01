e31fc5490ae27cd59dcfb20fed4a07304a1f38f2
Alison <zhangyinag@126.com>
2020-12-02 01:25:53 +0800

    license to 365

--- src/main/java/com/rawa/cloud/runner/data/PropertyDataGenerator.java
+++ src/main/java/com/rawa/cloud/runner/data/PropertyDataGenerator.java
@@ -15,7 +15,7 @@ public class PropertyDataGenerator implements DataGenerator{
     @Override
     public void generate() {
         String license = propertyService.getValue("license");
-        if (StringUtils.isEmpty(license)) propertyService.add("license", "UBLGK5n+d0C8fVIIkGIWzLdEIz5ueV4EOleO1tyHyJ/Jat8GtvhqEE/dAwfYw45MUnIoH5JIt429/KcFyo6iQ4SWR0eyt/Zwg6KURvNlSfJGfz8J9ciBlKfApJoBBNDJ7ioSIJDvHqBmEqzSqSPwETGmnYZ0y/F4GVUgOqB4bpk=");
+        if (StringUtils.isEmpty(license)) propertyService.add("license", "IuR6gG5cAgfvk2IxddTgVEkrPJKnLkcTaDJ74gt/LmX2ercpYowcNhAfiYSBoatEYsSH6/FOr/ltjH/nloEXsvryikm9vQWNUkRnyspbLbIVDEnOx6NtrCVXBiag+t2CcEhSmXjqvXONZxDOnD049MkziptX9Ge+Wiv64OuJdHo=");
         String pubKey = propertyService.getValue("pub_key");
         if (StringUtils.isEmpty(license)) propertyService.add("pub_key", "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCIV/RvjK870Fip0Dt7jjMlchjT0UBPLRSBRpUitlrdshu+3NeImXaLHl24j7kZOz7yVtXNuow6VUTENyTYMhAMaqHum7AmR+35kpIXtNNNzd5u0HM/KxanJc3/uQMrTXvauox0qrxEyCQ3mNg7RBdWiuqPXHav5OE2BtjMZIR4uwIDAQAB");
     }
